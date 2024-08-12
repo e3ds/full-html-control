1463c09edafd77f822c731cdd1f0a2935f9087f5
OmyE3D <Omy@eagle3dstreaming.com>
2024-08-13 00:39:03 +0600

    streaming UI without node js server

--- scripts_demo/postLoadScripts.js
+++ scripts_demo/postLoadScripts.js
@@ -1,4 +1,36 @@
 
+
+window.onload = async function(){
+    const response = await fetch('../config.json');
+    const {clientUserName, tokenExpiryDuration, apiKey, app, config} = await response.json();
+
+    fetch(
+        "https://token.eaglepixelstreaming.com/api/v1/token/create",
+        {
+            method: "POST",
+            body: JSON.stringify({
+            "object": {
+                "core": {
+                "domain": "connector.eagle3dstreaming.com",
+                "userName": clientUserName,
+                "appName": app,
+                "configurationName": config
+                },
+                "expiry": tokenExpiryDuration,
+                "configurationToOverride": {}
+            },
+            "client": clientUserName
+            }),
+            headers: {
+            "Authorization": "Auth " + apiKey,
+            "Content-Type": "application/json"
+            }
+        }
+        ).then(res=> res.json())
+        .then(data=> e3ds_controller.main(data.token, clientUserName))
+        .catch(error=> console.error(error));
+}
+
 e3ds_controller.callbacks.onDataChannelOpen=function ()
 {
     console.log("ob-onDataChannelOpen");
@@ -25,8 +57,6 @@ e3ds_controller.callbacks.onResponseFromUnreal=function (descriptor)
 {
     console.log("ob-onResponseFromUnreal");
     console.log("UnrealResponse: "+descriptor);
-	
-	document.getElementById('LatencyStats').innerHTML=descriptor;
   
 }
 
@@ -65,12 +95,3 @@ e3ds_controller.callbacks.onHtmlBind=function ()
    
 
 }
-
-
-
-window.onload = function () 
-				{
-					
-					e3ds_controller.main(e3ds_session_token,clientUserName)
-					
-				}
